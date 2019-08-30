b423a3418183a735b5d789fb251ee0ddba72946d
Caio Teixeira <caio.xd63@hotmail.com>
2019-08-30 12:51:52 -0300

    Visto a questão de conexão com banco de dados

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import Event
 
 # Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'event_date', 'created_date')
+    list_filter = ('title', 'user')
+
+admin.site.register(Event, EventAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    event_date = models.DateTimeField()
+    created_date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    class Meta:
+        db_table = 'event'
+
+    def __str__(self):
+        return self.title
